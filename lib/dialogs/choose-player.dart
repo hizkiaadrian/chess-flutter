65b8ac637329b9a0c53b4a89989e3dacad1cee01
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-09-18 23:05:48 +0700

    #2 Create dialog widget to take user input on player color

---
+++ lib/dialogs/choose-player.dart
@@ -0,0 +1,52 @@
+import 'package:Chess/game-engine/utils/player.dart';
+import 'package:flutter/material.dart';
+
+class ChoosePlayerDialog extends StatefulWidget {
+  @override
+  _ChoosePlayerDialogState createState() => _ChoosePlayerDialogState();
+}
+
+class _ChoosePlayerDialogState extends State<ChoosePlayerDialog> {
+  Player chosenPlayer;
+
+  void setChosenPlayer(Player player) => setState(() => chosenPlayer = player);
+
+  Widget playerChoice(Player player) => Material(
+        child: InkWell(
+          onTap: () => setChosenPlayer(player),
+          child: Container(
+            height: 40.0,
+            width: 40.0,
+            decoration: BoxDecoration(
+              color: player == Player.Black ? Colors.black : Colors.white,
+              border: chosenPlayer == player
+                  ? Border.all(color: Colors.yellow, width: 3.0)
+                  : Border.all(color: Colors.black, width: 1.0),
+            ),
+          ),
+        ),
+      );
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text("Choose your color"),
+      content: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          playerChoice(Player.White),
+          SizedBox(
+            width: 5.0,
+          ),
+          playerChoice(Player.Black)
+        ],
+      ),
+      actions: [
+        FlatButton(
+            onPressed: () => Navigator.pop(context, chosenPlayer),
+            child: Text("Submit"))
+      ],
+    );
+  }
+}
