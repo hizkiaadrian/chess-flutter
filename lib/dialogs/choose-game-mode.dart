47b1773c78a9ee71fb3b5b45dc91413c869eb671
hizkiaadrian <hizkia.susanto@st-hughs.ox.ac.uk>
2020-09-20 01:57:50 +0700

    #2 Style choose game mode dialog

--- lib/dialogs/choose-game-mode.dart
+++ lib/dialogs/choose-game-mode.dart
@@ -4,34 +4,44 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class ChooseGameModeDialog extends StatelessWidget {
+  Widget gameModeButton({String displayText, Function func}) => RaisedButton(
+      onPressed: func,
+      padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 5.0),
+      child: Text(
+        displayText,
+        style: TextStyle(fontSize: 20.0),
+      ));
+
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text("Choose game mode"),
       content: Consumer<GameProvider>(
         builder: (context, gameProvider, child) => Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           mainAxisSize: MainAxisSize.min,
           children: [
-            FlatButton(
-                onPressed: () => {
+            gameModeButton(
+                displayText: "Vs AI",
+                func: () => {
                       gameProvider.setGameMode(GameMode.AgainstBot),
                       Navigator.pop(context, GameMode.AgainstBot),
-                    },
-                child: Text("Vs AI")),
-            FlatButton(
-                onPressed: () => {
+                    }),
+            gameModeButton(
+                displayText: "PvP same device",
+                func: () => {
                       gameProvider.setGameMode(GameMode.TwoPlayers),
                       Navigator.pop(context, GameMode.TwoPlayers),
-                    },
-                child: Text("2 Players")),
-            FlatButton(
-              onPressed: () => Navigator.pop(context, null),
-              child: Text("Cancel"),
-            ),
+                    }),
           ],
         ),
       ),
+      actions: [
+        FlatButton(
+          onPressed: () => Navigator.pop(context, null),
+          child: Text("Cancel"),
+        ),
+      ],
     );
   }
 }
